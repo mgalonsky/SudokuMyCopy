82bc7007ddbf8fc5ee3804c72d0bad566864a6ff
seratri <seratri@>
2014-09-12 11:38:30 -0700

    Created first button

--- Sudoku/MGSTViewController.m
+++ Sudoku/MGSTViewController.m
@@ -8,7 +8,9 @@
 
 #import "MGSTViewController.h"
 
-@interface MGSTViewController ()
+@interface MGSTViewController () {
+    UIButton* _button;
+}
 
 @end
 
@@ -18,12 +20,29 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Create button
+    CGRect frame = self.view.frame;
+    CGRect buttonFrame = CGRectMake(CGRectGetWidth(frame)*.5, CGRectGetHeight(frame)*.5, 50, 50);
+    _button = [[UIButton alloc] initWithFrame:buttonFrame];
+    _button.backgroundColor = [UIColor blueColor];
+    _button.tag = 1;
+    [self.view addSubview:_button];
+    
+    [_button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void) buttonPressed: (id) sender
+{
+    UIButton *button = (UIButton*) sender;
+    button.showsTouchWhenHighlighted = TRUE;
+    NSLog(@"Button %d was pressed", button.tag);
 }
 
 @end
